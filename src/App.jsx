438459435dbe7f425371b0b05f46df6c94abc8c0
shubham <shubhamsarkar2587@gmail.com>
2022-11-26 23:59:17 +0530

    Title: register page created Dev: Shubham Date: 26 Nov 2022

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import "./app.css";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div className="app">
-      <Product />
+      <Register />
     </div>
   )
 }
