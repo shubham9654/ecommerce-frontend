9ca58fc9b20eb47aba2fba1a85a0686cdcc7bfe5
shubham <shubhamsarkar2587@gmail.com>
2022-12-06 23:20:32 +0530

    Change access origin

--- src/services/api/common.api.js
+++ src/services/api/common.api.js
@@ -1,5 +1,7 @@
 export const baseUrl = "http://54.165.184.12:8080/api/v1";
 
 export const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*',
   'Content-Type': 'application/json'
 }
